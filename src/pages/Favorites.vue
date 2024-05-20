aa25cbfa3c481e08dbaf84b98ae8d815d28395cf
AladievM <143446484+AladievM@users.noreply.github.com>
2024-05-20 16:02:04 +0300

    Added my project

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+import CardList from '../components/CardList.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      'https://d3344241bc4c27aa.mokky.dev/favorites?_relations=items'
+    )
+    favorites.value = data.map((obj) => obj.item)
+  } catch (err) {
+    console.log(err)
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold mb-8" v-auto>Мои закладки</h2>
+  <CardList :items="favorites" is-favorites/>
+</template>
